9277d261b73d72760c285b3011d512cc7a94d389
DAKOTA <matujoa13@gmail.com>
2019-03-22 14:22:33 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
-const Enmap = require("enmap");
-const fs = require("fs");
+
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -13,30 +12,6 @@ client.on('ready', () => {
 });
 
 const client = new Discord.Client();
-const config = require("./config.json");
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
-client.config = config;
 
-fs.readdir("./events/", (err, file) => {
-  if (err) return console.error(err);
-  file.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
-
-client.commands = new Enmap();
-
-fs.readdir("./commands/", (err, file) => {
-  if (err) return console.error(err);
-  file.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
 
 client.login(process.env.BOT_TOKEN);
