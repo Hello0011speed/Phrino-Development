7c7a7096694f3fd3fb42ab67fe739776f548467b
DAKOTA <matujoa13@gmail.com>
2019-03-22 14:40:46 -0300

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const Enmap = require("enmap");
 const fs = require("fs");
 const { Collection } = require("discord.js");
-client.commands = new Collection();
+
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -27,7 +27,7 @@ fs.readdir("./events/", (err, files) => {
     });
   });
 
-client.commands = new Enmap();
+client.commands = new Collection();
 
 fs.readdir("./commands/", (err, files) => {
   if (err) return console.error(err);
