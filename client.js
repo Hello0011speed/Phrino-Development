9b7452c1f9544bdcfdeb591f5712b9158c312768
DAKOTA <matujoa13@gmail.com>
2019-03-22 14:09:28 -0300

    Update client.js

--- client.js
+++ client.js
@@ -2,6 +2,16 @@ const Discord = require("discord.js");
 const Enmap = require("enmap");
 const fs = require("fs");
 
+client.on('ready', () => {
+    console.log('I am ready!');
+    const embed = new Discord.RichEmbed()
+        .setTitle("Phrino Dev Reload...")
+        .setColor("#21E514")
+        .setAuthor("Phrino Dev", "https://media.discordapp.net/attachments/557338439046135850/558691033437831188/Photo_20190322_133821.png?width=300&height=300")
+        .setDescription("The bot was properly reloaded :white_check_mark:");
+    client.channels.get("557348230245908482").send({embed});
+});
+
 const client = new Discord.Client();
 const config = require("./config.json");
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
