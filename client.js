eff7358361499167aea1aeb664ac245f309caf4f
DAKOTA <matujoa13@gmail.com>
2019-03-22 16:24:15 -0300

    Update client.js

--- client.js
+++ client.js
@@ -1,8 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const Enmap = require("enmap");
-const fs = require("fs");
-
 
 
 client.on('ready', () => {
@@ -15,30 +12,6 @@ client.on('ready', () => {
     client.channels.get("557348230245908482").send({embed});
 })
 
-const config = require("./config.json");
-client.config = config;
-
-fs.readdir("./events/", (err, files) => {
-    if (err) return console.error(err);
-    files.forEach(file => {
-      const event = require(`./events/${file}`);
-      let eventName = file.split(".")[0];
-      client.on(eventName, event.bind(null, client));
-    });
-  });
-
-client.commands = new Collection();
-
-fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
-    client.commands.set(commandName, props);
-  });
-});
 
 
 client.login(process.env.BOT_TOKEN);
