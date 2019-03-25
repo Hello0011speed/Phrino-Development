0c9f0fcb1431ccdf3d3d7ac902414b616a16fa9d
DAKOTA <matujoa13@gmail.com>
2019-03-25 20:47:54 -0300

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -9,14 +9,14 @@ exports.run = async (client, message, args, level) => { // eslint-disable-line n
       .setTimestamp();
     const embeds = new Discord.RichEmbed()
       .setTitle("Phrino · Staff Direct Message Logs")
-      .setDescription(`Message: ` + messageToSend + `Customer: ` + userToSend + `\n Author: ` + message.author)
+      .setDescription(`Message: ` + messageToSend + `\n Author: ` + message.author)
       .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
       .setColor("#E51414")
       .setTimestamp();
     
     
     userToSend.send(embed);
-    client.channels.get("558064435449823242").send(embeds)
+    client.channels.get("558064435449823242").send(embeds);
     
     
   };
