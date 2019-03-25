69c880ac1bcec06bd37b165cb9295d59c2550675
DAKOTA <matujoa13@gmail.com>
2019-03-25 13:12:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const config = {
 
   permLevels: [
     // This is the lowest permisison level, this is for non-roled users.
-    { level: 0,
+    { level: 1,
       name: "User", 
       // Don't bother checking, just return true which allows them to execute any command their
       // level allows them to.
@@ -39,7 +39,7 @@ const config = {
     },
 
     // This is your permission level, the staff levels should always be above the rest of the roles.
-    { level: 2,
+    { level: 20,
       // This is the name of the role.
       name: "Moderator",
       // The following lines check the guild the message came from for the roles.
@@ -56,7 +56,7 @@ const config = {
       }
     },
 
-    { level: 3,
+    { level: 30,
       name: "Administrator", 
       check: (message) => {
         try {
@@ -68,7 +68,7 @@ const config = {
       }
     },
     // This is the server owner.
-    { level: 4,
+    { level: 60,
       name: "Server Owner", 
       // Simple check, if the guild owner id matches the message author's ID, then it will return true.
       // Otherwise it will return false.
@@ -77,7 +77,7 @@ const config = {
 
     // Bot Support is a special inbetween level that has the equivalent of server owner access
     // to any server they joins, in order to help troubleshoot the bot on behalf of owners.
-    { level: 8,
+    { level: 180,
       name: "Bot Staff",
       // The check is by reading if an ID is part of this array. Yes, this means you need to
       // change this and reboot the bot to add a support user. Make it better yourself!
@@ -85,7 +85,7 @@ const config = {
     },
 
     // Bot Admin has some limited access like rebooting the bot or reloading commands.
-    { level: 9,
+    { level: 254,
       name: "Co-Founders",
       check: (message) => config.admins.includes(message.author.id)
     },
@@ -93,7 +93,7 @@ const config = {
     // This is the bot owner, this should be the highest permission level available.
     // The reason this should be the highest level is because of dangerous commands such as eval
     // or exec (if the owner has that).
-    { level: 10,
+    { level: 255,
       name: "Founder", 
       // Another simple check, compares the message author id to the one stored in the config file.
       check: (message) => message.client.config.ownerID === message.author.id
