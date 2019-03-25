7ca1fd3a6b18770ed5395aa4b38247f8c1242b83
DAKOTA <matujoa13@gmail.com>
2019-03-25 20:04:44 -0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -4,26 +4,14 @@ require("moment-duration-format");
 
 exports.run = (client, message, args, level) => { // eslint-disable-line no-unused-vars
   const duration = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
-  const embed = new Discord.RichEmbed()
-    .setTitle("Phrino · Stats")
-    .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
-    .setColor("#E51414")
-    .setDescription(`\n • Mem Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB \n • Uptime: ${duration} \n Users: ${client.users.size.toLocaleString()} \n Servers: ${client.guilds.size.toLocaleString()} \n Channels: ${client.channels.size.toLocaleString()}`);
-    message.channel.send({embed: {
-      color: 3447003,
-      author: {
-        name: "Phrino",
-        icon_url: "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?"
-      },
-      title: "Phrino · Stats",
-      description: `\n • Mem Usage: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB \n • Uptime: ${duration} \n Users: ${client.users.size.toLocaleString()} \n Servers: ${client.guilds.size.toLocaleString()} \n Channels: ${client.channels.size.toLocaleString()}`,
-      timestamp: new Date(),
-      footer: {
-        icon_url: client.user.avatarURL,
-        text: "Phrino"
-      }
-    }
-  });
+  message.channel.send(`= STATISTICS =
+• Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+• Uptime     :: ${duration}
+• Users      :: ${client.users.size.toLocaleString()}
+• Servers    :: ${client.guilds.size.toLocaleString()}
+• Channels   :: ${client.channels.size.toLocaleString()}
+• Discord.js :: v${version}
+• Node       :: ${process.version}`, {code: "asciidoc"});
 };
 
 exports.conf = {
