0148e60042352bb5404387fc53aafcae4702bdb6
DAKOTA <matujoa13@gmail.com>
2019-03-25 20:37:18 -0300

    Update mylevel.js

--- commands/mylevel.js
+++ commands/mylevel.js
@@ -1,6 +1,14 @@
+const Discord = require ("discord.js");
+
 exports.run = async (client, message, args, level) => {
     const friendly = client.config.permLevels.find(l => l.level === level).name;
-    message.reply(`Your permission level is: ${level} - ${friendly}`);
+    const embed = new Discord.RichEmbed()
+    .setTitle("Phrino · Level")
+    .setDescription(`\n • Permission Level: ${level} \n • Permission Rank: ${friendly} `)
+    .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+    .setColor("#E51414")
+    .setTimestamp();
+message.channel.send(embed);
   };
   
   exports.conf = {
