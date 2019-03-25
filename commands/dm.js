255cc0e59afdfe2cb49a428c2e1c5cd1e1a4fdde
DAKOTA <matujoa13@gmail.com>
2019-03-25 20:46:18 -0300

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -1,8 +1,24 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
     let messageToSend = message.content.split(" ").slice(2).join(" ");
     let userToSend = message.mentions.users.first();
-
-    userToSend.send(messageToSend);
+    const embed = new Discord.RichEmbed()
+      .setTitle("Phrino · Staff Direct Message")
+      .setDescription(`Message: ` + messageToSend + `\n Author: ` + message.author)
+      .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+      .setColor("#E51414")
+      .setTimestamp();
+    const embeds = new Discord.RichEmbed()
+      .setTitle("Phrino · Staff Direct Message Logs")
+      .setDescription(`Message: ` + messageToSend + `Customer: ` + userToSend + `\n Author: ` + message.author)
+      .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+      .setColor("#E51414")
+      .setTimestamp();
+    
+    
+    userToSend.send(embed);
+    client.channels.get("558064435449823242").send(embeds)
+    
+    
   };
   
   exports.conf = {
