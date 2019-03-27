deefa694cac0127b5408ead138d268210f8cf482
DAKOTA <matujoa13@gmail.com>
2019-03-26 21:30:06 -0300

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,63 @@
+const Discord = require ("discord.js");
+
+exports.run = async (client, message, args, level) => {
+    const nop = new Discord.RichEmbed()
+        .setTitle("Phrino · Ban")
+        .setDescription(`Please specify the user to ban.`)
+        .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+        .setColor("#E51414")
+        .setTimestamp();
+    const self = new Discord.RichEmbed()
+        .setTitle("Phrino · Ban")
+        .setDescription(`You can not ban yourself!`)
+        .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+        .setColor("#E51414")
+        .setTimestamp();
+    const supe = new Discord.RichEmbed()
+        .setTitle("Phrino · Ban")
+        .setDescription(`This user is more powerful than both of us, unfortunately I can not ban it! :sob:`)
+        .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+        .setColor("#E51414")
+        .setTimestamp();
+    const user = message.mentions.members.first() || message.guild.members.get(args[0]);
+    let reason = args.slice(1).join(" ");
+    if (!user) return reply({nop});
+    if (!reason) reason = "None Provided";
+
+    if (user.id === message.author.id) return reply({self});
+    if (!user.bannable) return reply({supe});
+
+    try {
+        user.ban(reason);
+      } catch (e) {
+        const emebeddd = new Discord.RichEmbed()
+            .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+            .setColor("#E51414")
+            .setTimestamp()
+            .setTitle("Phrino · Error")
+            .setDescription(`I couldn't ban the specified user! \n • Reason: ${e}`);
+        return message.channel.send({emebeddd});
+      }
+      const banned = new Discord.RichEmbed()
+            .setAuthor("Phrino", "https://cdn.discordapp.com/icons/528818044668215299/6d1ccd655df1c562ef4f43835597fe10.png?")
+            .setColor("#E51414")
+            .setTimestamp()
+            .setTitle("Phrino · Ban")
+            .setDescription(`• Banned user: ${user.user.tag} \n Moderator: ` + message.author);
+      return reply({banned});
+
+  };
+  
+  exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "Administrator"
+};
+  
+  exports.help = {
+    name: "ban",
+    category: "Moderation",
+    description: "Bans an user from your server.",
+    usage: "ban"
+  };
