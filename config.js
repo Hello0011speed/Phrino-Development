e692334b8189d859c17dc1b4153ae784742c3013
DAKOTA <matujoa13@gmail.com>
2019-04-05 21:33:16 -0300

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const config = {
   
   "defaultSettings" : {
     "prefix": "..",
-    "modLogChannel": "mod-log",
+    "modLogChannel": "logs",
     "modRole": "Moderator",
     "adminRole": "Administrator",
     "systemNotice": "true", // This gives a notice when a user tries to run a command that they do not have permission to use.
